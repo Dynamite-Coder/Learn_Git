e41a801117006bf58eca64b13680c42d039c12a9
Pavan M Malashetti <pavanmalshetti@gmail.com>
2021-12-27 17:04:31 +0530

    Added sum variable in add.cpp

--- Add.cpp
+++ Add.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-    int a = 5, b = 3;
-    cout<<"Sum = "<<a+b<<endl;
+    int a = 5, b = 3,sum;
+    sum = a+b;
+    cout<<"Sum = "<<sum<<endl;
     return 0;
 }
